d4d2902f5c1b83fbc2303e1888d0f8e374d5d693
D-Big-B <prjjwlbhatt@gmail.com>
2022-04-19 04:08:16 +0530

    Updated

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/username-a4'),
+      dir: require('path').join(__dirname, './coverage/choudhvi-a4'),
       subdir: '.',
       reporters: [
         { type: 'html' },
